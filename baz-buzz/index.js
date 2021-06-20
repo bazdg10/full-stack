679bd4020a0c77ee3eeb1eb07e2b00c414586c40
bazdg10 <bazdg10@gmail.com>
2021-06-20 13:26:25 +0530

    Trying to access Geo Location Feature

---
+++ baz-buzz/index.js
@@ -0,0 +1,73 @@
+const { error } = require('console');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/user');
+app.listen(3000);
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+
+/*const users = [
+    {name: "Biswanath", location: "Kolkata"},
+    {name: "Arya", location: "Delhi"}
+]*/
+
+const userURI = 'mongodb+srv://bazdg10:test1234@cluster0.j1a1t.mongodb.net/BazBuzz?retryWrites=true&w=majority';
+mongoose.connect(userURI, { useNewUrlParser: true, useUnifiedTopology: true } )
+        .then(console.log(`Connected to db`)).catch((err)=> console.log(error));
+
+app.use((req, res, next)=> {
+    console.log(req.body);
+    console.log(req.hostname);
+    next();
+})
+// MongoDB TCs
+// app.get('/add-user', (req, res)=> {
+//     const user = new User({
+//         name: 'Abhijit',
+//         location: 'Abu Dhabi'
+//     });
+//     user.save()
+//         .then( result => res.send(result))
+//         .catch( error => res.send(error))
+// })
+//
+app.get('/get-all', (req, res)=> {
+    User.find()
+        .then( result => {
+            res.render('userList', { title: 'Search Results', users: result });
+        })  
+        .catch( err => {
+            console.log(err);
+        })
+})
+
+
+app.post('/add-user', (req, res)=> {
+    const user = new User(req.body);
+    user.save().then(result => res.redirect('get-all'))
+        .catch(err => console.log(err));
+    
+});
+
+app.post('/lookup', (req, res) => {
+    if (req.body.search==="All")  res.redirect('/get-all');
+    else{
+    User.find({ name: req.body.search})
+    .then( result => {
+        res.render('userList', { title: 'Search Results', users: result });
+    })  
+    .catch( err => {
+        console.log(err);
+    })
+    }
+});
+
+app.get('/', (req, res)=> {
+    res.render('home', {title: 'Home'});
+});
+
+
+app.use((req, res)=> {
+    res.status(404).render('404', {title: 'Error'});
+})
---
+++ baz-buzz/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true } );
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
