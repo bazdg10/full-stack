abbed3d1ef728e1f367554d50afcc93038924bd6
Biswanath Dasgupta <71487097+bazdg10@users.noreply.github.com>
2021-06-22 04:43:26 +0530

    Update index.js

--- baz-buzz/index.js
+++ baz-buzz/index.js
@@ -16,8 +16,8 @@ app.use(express.urlencoded({ extended: true }));
     {name: "Arya", location: "Delhi"}
 ]*/
 
-
-const userURI = 'mongodb+srv://bazdg10:test1234@cluster0.j1a1t.mongodb.net/BazBuzz?retryWrites=true&w=majority';
+// ASK IF  testing
+const userURI = 'mongodb+srv://#####################################################################';
 mongoose.connect(userURI, { useNewUrlParser: true, useUnifiedTopology: true } )
         .then(console.log(`Connected to db`)).catch((err)=> console.log(error));
 
